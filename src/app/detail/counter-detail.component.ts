f02b038a9b47073be695972087b4c969e3198997
Stephen White <stephenw1016@gmail.com>
2017-05-07 13:09:56 -0400

    added async pipe

--- src/app/detail/counter-detail.component.ts
+++ src/app/detail/counter-detail.component.ts
@@ -2,14 +2,17 @@ import {Component, OnInit} from '@angular/core';
 
 import NumberService from "../shared/number.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'counter-detail-component',
   templateUrl: './counter-detail.component.html',
+  styleUrls: ['./counter-detail.component.css'],
   providers: [NumberService]
 })
 export default class CounterDetailComponent implements OnInit {
-  private total: number;
+  // private total: number;
+  private total: Observable<number>;
 
   constructor (
     private numberService: NumberService,
@@ -17,15 +20,18 @@ export default class CounterDetailComponent implements OnInit {
   ) {}
 
   ngOnInit () {
+    this.total =  Observable.of(0);
+
     this.route.queryParams.subscribe((params: Params) => {
       console.log('total >>> ', params.start);
+      this.total = Observable.of(params.start);
     });
 
-    this.total = 0;
+    // this.total = 0;
 
-    setInterval(() => {
-      this.total += this.numberService.getTotal();
-    }, 5000);
+    // setInterval(() => {
+    //   this.total += this.numberService.getTotal();
+    // }, 5000);
   }
 
 }
